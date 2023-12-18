cf3eeb9ada12b51109e5c93058ca350ab5eba23c
Michael Carenzo <79934822+mikecarenzo@users.noreply.github.com>
2023-12-18 10:57:33 -0500

    update serializer to align with new `UniversalSchemaMappingFile` format

--- src/mappings_editor/src/assets/configuration/app.config.serializer.ts
+++ src/mappings_editor/src/assets/configuration/app.config.serializer.ts
@@ -1,5 +1,5 @@
 import { MappingFileSerializer } from "../scripts/Application";
-import type { MappingFileExport } from "../scripts/MappingFileAuthority";
+import type { MappingFileExport, MappingObjectExport } from "../scripts/MappingFileAuthority";
 
 export class UniversalSchemaMappingFileSerializer extends MappingFileSerializer {
 
@@ -18,38 +18,33 @@ export class UniversalSchemaMappingFileSerializer extends MappingFileSerializer 
      */
     public serialize(file: MappingFileExport): string {
         // Parse mapping objects
-        const mapping_objects = [];
+        const mapping_objects: UniversalSchemaMappingObject[] = [];
         for(const obj of file.mapping_objects) {
             const fsv = file.source_version,
                   ftv = file.target_version,
                   osv = obj.source_version,
                   otv = obj.target_version;
             mapping_objects.push({
-                author                  : obj.author,
-                contact                 : obj.author_contact,
-                comments                : obj.comments,
+                author                  : obj.author ?? undefined,
+                contact                 : obj.author_contact ?? undefined,
+                comments                : obj.comments ?? undefined,
                 references              : obj.references,
                 attack_object_id        : obj.target_id,
                 attack_object_name      : obj.target_text,
-                attack_object_version   : otv !== undefined && otv !== ftv ? otv : undefined,
+                attack_object_version   : otv !== null && otv !== ftv ? otv : undefined,
                 capability_id           : obj.source_id,
                 capability_description  : obj.source_text,
-                capability_version      : osv !== undefined && osv !== fsv ? osv : undefined,
+                capability_version      : osv !== null && osv !== fsv ? osv : undefined,
                 mapping_type            : obj.mapping_type,
-                group                   : obj.mapping_group,
-                status                  : obj.mapping_status
+                score_category          : obj.score_category ?? undefined,
+                score_value             : obj.score_value ?? undefined,
+                related_score           : obj.related_score ?? undefined,
+                group                   : obj.mapping_group ?? undefined,
+                status                  : obj.mapping_status ?? undefined
             });
         }
         // Parse domain and version
         const [attackDomain, attackVersion] = file.target_version.split(/@/);
-        // Parse mapping types
-        const mapping_types = Object.entries(file.mapping_types).map(
-            ([id, obj]) => ({ id, ...obj })
-        )
-        // Parse mapping groups
-        const groups = Object.entries(file.mapping_groups).map(
-            ([id, name]) => ({ id, name })
-        )
         // Parse metadata
         const unifiedSchemaFile: UniversalSchemaMappingFile = {
             metadata: {
@@ -63,8 +58,8 @@ export class UniversalSchemaMappingFileSerializer extends MappingFileSerializer 
                 last_update                      : file.modified_date.toString(),
                 mapping_framework                : file.source_framework,
                 mapping_framework_version        : file.source_version,
-                mapping_types,
-                groups
+                mapping_types                    : file.mapping_types,
+                groups                           : file.mapping_groups
             },
             mapping_objects
         }
@@ -79,54 +74,67 @@ export class UniversalSchemaMappingFileSerializer extends MappingFileSerializer 
     public deserialize(file: string): MappingFileExport {
         const json = JSON.parse(file) as UniversalSchemaMappingFile;
         // Parse mapping objects
-        const mapping_objects = [];
+        const mapping_objects: MappingObjectExport[] = [];
         for(const obj of json.mapping_objects) {
             mapping_objects.push({
-                source_id        : obj.capability_id,
-                source_text      : obj.capability_description,
-                source_version   : obj.capability_version,
-                target_id        : obj.attack_object_id,
-                target_text      : obj.attack_object_name,
-                target_version   : obj.attack_object_version,
-                mapping_type     : obj.mapping_type,
-                mapping_group    : obj.group,
-                mapping_status   : obj.status ?? "complete",
-                author           : obj.author,
-                author_contact   : obj.contact,
-                references       : obj.references,
-                comments         : obj.comments
+                source_id           : obj.capability_id,
+                source_text         : obj.capability_description,
+                source_version      : obj.capability_version ?? null,
+                source_framework    : null,
+                target_id           : obj.attack_object_id,
+                target_text         : obj.attack_object_name,
+                target_version      : obj.attack_object_version ?? null,
+                target_framework    : null,
+                author              : obj.author ?? null,
+                author_contact      : obj.contact ?? null,
+                author_organization : null,
+                references          : obj.references,
+                comments            : obj.comments ?? null,
+                mapping_type        : obj.mapping_type,
+                mapping_group       : obj.group ?? null,
+                mapping_status      : obj.status ?? "in_progress",
+                score_category      : obj.score_category ?? null,
+                score_value         : obj.score_value ?? null,
+                related_score       : obj.related_score ?? null
             });
         }
         const meta = json.metadata;
-        // Parse mapping types
-        const mapping_types = Object.fromEntries(meta.mapping_types.map(
-            ({id, name, description}) => [id, { name, description }]
-        ));
-        // Parse mapping groups
-        const mapping_groups = Object.fromEntries(meta.groups.map(
-            ({id, name}) => [id, name]
-        ));
         // Define mapping statuses
         const mapping_statuses = {
             "complete"     : "Complete",
             "in_progress"  : "In Progress",
             "non_mappable" : "Non-Mappable"
-        }
+        };
+        // Define score categories
+        const score_categories = {
+            "protect"      : "Protect",
+            "detect"       : "Detect",
+            "respond"      : "Respond"
+        };
+        // Define score values
+        const score_values = {
+            "minimal"      : "Minimal",
+            "partial"      : "Partial",
+            "significant"  : "Significant"
+        };
         // Parse metadata
         const mappingFileExport = { 
-            version               : meta.mapping_version,
-            source_framework      : meta.mapping_framework,
-            source_version        : meta.mapping_framework_version,
-            target_framework      : "mitre_attack",
-            target_version        : `${ meta.technology_domain }@${ meta.attack_version }`,
-            author                : meta.author,
-            author_contact        : meta.contact,
-            author_organization   : meta.organization,
-            creation_date         : new Date(meta.creation_date),
-            modified_date         : new Date(meta.last_update),
-            mapping_types,
-            mapping_groups,
+            version                : meta.mapping_version,
+            source_framework       : meta.mapping_framework,
+            source_version         : meta.mapping_framework_version,
+            target_framework       : "mitre_attack",
+            target_version         : `${ meta.technology_domain }@${ meta.attack_version }`,
+            author                 : meta.author,
+            author_contact         : meta.contact,
+            author_organization    : meta.organization,
+            creation_date          : new Date(meta.creation_date),
+            modified_date          : new Date(meta.last_update),
+            mapping_types          : meta.mapping_types,
+            mapping_groups         : meta.groups,
             mapping_statuses,
+            score_categories,
+            score_values,
+            default_mapping_status : "in_progress",
             mapping_objects
         }
         return mappingFileExport;
@@ -150,28 +158,31 @@ type UniversalSchemaMappingFile = {
         creation_date: string,
         last_update: string,
         mapping_types: {
-            id: string,
-            name: string,
-            description: string
-        }[],
-        groups: { 
-            id: string,
-            name: string
-        }[]
+            [key: string]: { name: string, description: string }   
+        },
+        groups: { [key: string]: string }
     },
-    mapping_objects: {
-        author: string | null,
-        contact: string | null,
-        comments: string | null,
-        references: string[],
-        attack_object_id: string | null,
-        attack_object_name: string | null,
-        attack_object_version?: string,
-        capability_id: string | null,
-        capability_description: string | null,
-        capability_version?: string,
-        mapping_type: string | null,
-        group: string | null,
-        status: string | null
-    }[]
+    mapping_objects: UniversalSchemaMappingObject[]
+}
+
+/**
+ * The Universal Schema Mapping Object Format
+ */
+type UniversalSchemaMappingObject = {
+    author?: string,
+    contact?: string,
+    comments?: string,
+    references: string[],
+    attack_object_id: string | null,
+    attack_object_name: string | null,
+    attack_object_version?: string,
+    capability_id: string | null,
+    capability_description: string | null,
+    capability_version?: string,
+    mapping_type: string | null,
+    score_category?: string,
+    score_value?: string,
+    related_score?: string,
+    group?: string,
+    status?: string
 }
