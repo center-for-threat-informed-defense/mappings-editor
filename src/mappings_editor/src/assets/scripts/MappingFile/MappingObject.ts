ea844e5860aa8ef1f01e1684abf0f8e775076e6a
Michael Carenzo <79934822+mikecarenzo@users.noreply.github.com>
2024-02-27 09:44:30 -0500

    fix bug preventing `MappingObject` from using pre-configured `references`

--- src/mappings_editor/src/assets/scripts/MappingFile/MappingObject.ts
+++ src/mappings_editor/src/assets/scripts/MappingFile/MappingObject.ts
@@ -125,7 +125,7 @@ export class MappingObject {
         this.author = config.author;
         this.authorContact = config.authorContact;
         this.authorOrganization  = config.authorOrganization;
-        this.references = new ListProperty(
+        this.references = config.references ?? new ListProperty(
             "References",
             new ListItem(new Map([
                 ["url", new StringProperty("URL")]
