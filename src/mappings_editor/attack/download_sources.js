fdc08cce44cbcdc820c2befce9ac249ecb01f9b1
Michael Carenzo <79934822+mikecarenzo@users.noreply.github.com>
2025-07-14 11:57:12 -0400

    add support for ATT&CK v17.0 and v17.1

--- src/mappings_editor/attack/download_sources.js
+++ src/mappings_editor/attack/download_sources.js
@@ -8,11 +8,12 @@ const BASE_URL = "https://raw.githubusercontent.com/mitre-attack/attack-stix-dat
  * Enterprise Sources
  */
 const ENTERPRISE_SOURCES = [
-    "16.1", "16.0", "15.1", "15.0",
-    "14.1", "14.0", "13.1", "13.0",
-    "12.1", "12.0", "11.2", "11.1",
-    "11.0", "10.1", "10.0", "9.0",
-    "8.2",  "8.1", "8.0"
+    "17.1", "17.0", "16.1", "16.0",
+    "15.1", "15.0", "14.1", "14.0",
+    "13.1", "13.0", "12.1", "12.0",
+    "11.2", "11.1", "11.0", "10.1",
+    "10.0", "9.0",  "8.2",  "8.1",
+    "8.0"
 ].map(version => ({
     url              : `${BASE_URL}/enterprise-attack/enterprise-attack-${ version }.json`,
     frameworkId      : "mitre_attack_enterprise",
@@ -23,11 +24,12 @@ const ENTERPRISE_SOURCES = [
  * ICS Sources
  */
 const ICS_SOURCES = [
-    "16.1", "16.0", "15.1", "15.0",
-    "14.1", "14.0", "13.1", "13.0",
-    "12.1", "12.0", "11.3", "11.2",
-    "11.1", "11.0", "10.1", "10.0",
-    "9.0",  "8.2",  "8.1",  "8.0"
+    "17.1", "17.0", "16.1", "16.0",
+    "15.1", "15.0", "14.1", "14.0",
+    "13.1", "13.0", "12.1", "12.0",
+    "11.3", "11.2", "11.1", "11.0",
+    "10.1", "10.0", "9.0",  "8.2",
+    "8.1",  "8.0"
 ].map(version => ({
     url              : `${BASE_URL}/ics-attack/ics-attack-${ version }.json`,
     frameworkId      : "mitre_attack_ics",
@@ -38,11 +40,11 @@ const ICS_SOURCES = [
  * Mobile Sources
  */
 const MOBILE_SOURCES = [
-    "16.1", "16.0", "15.1", "15.0",
-    "14.1", "14.0", "13.1", "13.0",
-    "12.1", "12.0", "11.3", "10.1",
-    "10.0", "9.0", "8.2",  "8.1",
-    "8.0"
+    "17.1", "17.0", "16.1", "16.0",
+    "15.1", "15.0", "14.1", "14.0",
+    "13.1", "13.0", "12.1", "12.0",
+    "11.3", "10.1", "10.0", "9.0",
+    "8.2",  "8.1",  "8.0"
 ].map(version => ({
     url              : `${BASE_URL}/mobile-attack/mobile-attack-${ version }.json`,
     frameworkId      : "mitre_attack_mobile",
